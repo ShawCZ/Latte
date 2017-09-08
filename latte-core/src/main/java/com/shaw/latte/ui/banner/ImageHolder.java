87c209094fe2f1c857014b5895a1af64ae6d3626
shaw <2118772379@qq.com>
2017-09-08 15:05:15 +0800

    更新Glide图片加载

--- latte-core/src/main/java/com/shaw/latte/ui/banner/ImageHolder.java
+++ latte-core/src/main/java/com/shaw/latte/ui/banner/ImageHolder.java
@@ -8,15 +8,22 @@ import android.widget.ImageView;
 import com.bigkoo.convenientbanner.holder.Holder;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.shaw.latte.R;
 
 /**
  * Created by shaw on 2017/9/8.
  */
 
-public class ImageHolder implements Holder<String>{
+public class ImageHolder implements Holder<String> {
 
     private AppCompatImageView mImageView = null;
+
+    //设置图片加载策略
+    private static final RequestOptions BANNER_OPTIONS = new RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .dontAnimate()
+            .centerCrop();
 
     @Override
     public View createView(Context context) {
@@ -28,9 +35,7 @@ public class ImageHolder implements Holder<String>{
     public void UpdateUI(Context context, int position, String data) {
         Glide.with(context)
                 .load(data)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .dontAnimate()
-                .centerCrop()
+                .apply(BANNER_OPTIONS)
                 .into(mImageView);
 
     }
